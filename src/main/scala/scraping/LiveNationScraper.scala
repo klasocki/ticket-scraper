bb1a054c0a721a3e6a4dc0a81a95741f87960c10
Lukasz Kowalski <lukowalski97@gmail.com>
2019-06-06 19:16:16 +0200

    Update LiveNationScraper.scala
    scrappedList+

--- src/main/scala/scraping/LiveNationScraper.scala
+++ src/main/scala/scraping/LiveNationScraper.scala
@@ -10,5 +10,22 @@ class LiveNationScraper {
   def ticketsAvailable(event: Event): Boolean = {
     false
   }
+  
+  def scrappedList(pageNumber: String): List[(String,String)]={
 
+    val browser = JsoupBrowser()
+
+    val doc = browser.get("https://www.livenation.pl/event/allevents?page=" + pageNumber )
+
+    var tmp = doc >> element(".allevents__eventlist")
+
+    var list = tmp >> elementList("li")
+    var listNames = list.map(_ >> (allText(".result-info__headliners"), allText(".result-card__actions")))
+    for (elem <- listNames) {
+      println("Tytul: " + elem)
+    }
+    
+    return listNames
+
+  }
 }
