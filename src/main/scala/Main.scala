987e744839580991691b9485d956e5d392ecbc96
klasocki <karolasocki@gmail.com>
2019-06-11 19:05:10 +0200

    Add inital search engine

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,11 @@
 import scraping.LiveNationScraper
+import scraping.SearchEngine
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("Live Nation ticket scraper")
     val scraper = new LiveNationScraper("https://www.livenation.pl")
-    for (elem <- scraper.getEventList) {
+    for (elem <- SearchEngine.searchForEvents(scraper.getEventList, "Impact")) {
       println(elem)
     }
   }
---
+++ src/main/scala/scraping/SearchEngine.scala
@@ -0,0 +1,28 @@
+package scraping
+
+import events.Event
+
+import scala.math.min
+
+object SearchEngine {
+  private def LevenshteinDist(a: String, b: String): Int =
+    ((0 to b.length).toList /: a)((prev, x) =>
+      (prev zip prev.tail zip b).scanLeft(prev.head + 1) {
+        case (h, ((d, v), y)) => min(min(h + 1, v + 1), d + (if (x == y) 0 else 1))
+      }) last
+
+  private def matches(string: String, pattern: String): Boolean = {
+    val a = string.toLowerCase.trim
+    val b = pattern.toLowerCase.trim
+    val minPrefixLen = 5
+    val maxPrefixDist = b.length / 5
+    val maxFullDist = b.length / 3
+    for (i <- minPrefixLen to a.length){
+      if (LevenshteinDist(a.take(i), b) < maxPrefixDist) return true
+    }
+    LevenshteinDist(a, b) < maxFullDist
+  }
+
+  def searchForEvents(events: List[Event], pattern: String): List[Event] =
+    events.filter(e => matches(e.name, pattern))
+}
